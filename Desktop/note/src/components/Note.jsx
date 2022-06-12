892c709406592b4738ac62a3c2627440ec5917af
Coco <Coco@192.168.1.5>
2022-06-13 00:14:26 +0200

    note

---
+++ Desktop/note/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Fatma</h1>
+      <p>you can do it, keep going.</p>
+    </div>
+  );
+}
+
+export default Note;
